f93ba853b493846b715609c5d2aa07c471b273fc
adrianpalmquist <adrianpalmquist@gmail.com>
2015-04-20 15:12:28 +0200

    move bullet to ext

--- bullet/bullet3-Bullet-2.83-alpha/build3/bin2cpp.bat
+++
@@ -1,7 +0,0 @@
-
-rem @echo off
-
-
-premake4 --file=bin2cpp.lua --binfile="../btgui/FontFiles/OpenSans.ttf" --cppfile="../btgui/FontFiles/OpenSans.cpp" --stringname="OpenSansData" bin2cpp
-
-pause
--- bullet/bullet3-Bullet-2.83-alpha/build3/vs2010.bat
+++
@@ -1,6 +0,0 @@
-
-rem premake4 --with-pe  vs2010
-premake4  vs2010
-
-mkdir vs2010\cache
-pause
