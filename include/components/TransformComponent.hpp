e3c8230776d7eb8928747ac42912c796cf34872f
Benjamin Wiberg <benjaminwiberg@gmail.com>
2015-04-23 13:03:40 +0200

    Updated TransformComp mat4 constructor to extract transl and rot.
    Former-commit-id: 9ec95c6aa7208841db458199a81bc5fe3528c14a

--- include/components/TransformComponent.hpp
+++ include/components/TransformComponent.hpp
@@ -9,6 +9,7 @@
 #include "glm/gtc/quaternion.hpp"
 #include "glm/gtx/quaternion.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
 
 #include <entityx/entityx.h>
 
@@ -27,16 +28,33 @@ namespace sw {
 
         TransformComponent(glm::mat4 local_transform)
                 : local_(local_transform) {
-            // TODO: extract position_ and rotation_
+            glm::vec3 temp3;
+            glm::vec4 temp4;
+            glm::decompose(local_, temp3, orientation_, position_, temp3, temp4);
+
+            /*
+            std::cout << "Position extracted from transformation matrix: " << std::endl
+                      << "{" << position_[0] << ", " << position_[1] << ", " << position_[2] << "}" << std::endl;
+
+            glm::vec3 temp_rot = glm::eulerAngles(orientation_) * 3.14159f / 180.f;
+
+            std::cout << "Rotation extracted from transformation matrix: " << std::endl
+                      << "{" << temp_rot[0] << ", " << temp_rot[1] << ", " << temp_rot[2] << "}" << std::endl;
+            std::cout << std::endl;
+            */
+
             is_dirty_ = true;
         }
 
         /** Public member variables for convenience **/
         glm::vec3 position_;
-        glm::vec3 rotation_; // Contains: {rotX, rotY, rotZ}
         glm::quat orientation_;
         glm::mat4 local_;
         glm::mat4 cached_world_;
+
+
+        // TODO: remove this
+        glm::vec3 rotation_; // Contains: {rotX, rotY, rotZ}
 
         // Set this to true if position or rotation has been updated
         bool is_dirty_;
