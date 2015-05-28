7cf32f680a67be2024056497ad928386c7230212
johanordin <johno855@student.liu.se>
2015-05-28 16:27:22 +0200

    working movment with PS controller
    Former-commit-id: 1b0d2118f96e0bb64271f6936aec4a3ddcee026c

--- include/systems/InputSystem.hpp
+++ include/systems/InputSystem.hpp
@@ -9,6 +9,7 @@
 #include "entityx/entityx.h"
 #include "SDL.h"
 #include <math.h>       /* fmax */
+#include <cmath>
 
 #include "events/JumpEvent.hpp"
 #include "events/MovementEvent.hpp"
@@ -109,43 +110,53 @@ namespace sw {
 
             // x-axis for left analog joystick
             if (sdlEvent.axis == 0) {
-                right = sdlEvent.value / MAX_VALUE_CONTROLLER;
-                //std::cout << "--> value " << sdlEvent.value << std::endl;
+                //right = sdlEvent.value / MAX_VALUE_CONTROLLER;
 
-                if (right > 0.1f) {
-                    right = 1.0f;
-                }
-                if (right < -0.1f) {
-                    right = -1.0f;
-                }
 
                 // returns the normalized value between -1 and 1.
                 right = fmaxf(-1.0f, (float) sdlEvent.value / MAX_VALUE_CONTROLLER);
-
+                //std::cout << "--> right " << sdlEvent.value << std::endl;
             }
             // z-axis for left analog joystick
             if (sdlEvent.axis == 1) {
-                forward = sdlEvent.value / MAX_VALUE_CONTROLLER;
-                std::cout << " forward: " << forward << std::endl;
+                //forward = sdlEvent.value / MAX_VALUE_CONTROLLER;
 
-                if (forward > 0.1f) {
-                    forward = -1.0f;
-                }
-                if (forward < -0.1f) {
-                    forward = 1.0f;
-                }
 
                 // returns the normalized value between -1 and 1.
-                forward = fmaxf(-1.0f, (float) sdlEvent.value / MAX_VALUE_CONTROLLER);
-
+                forward = -fmaxf(-1.0f, (float) sdlEvent.value / MAX_VALUE_CONTROLLER);
+                //std::cout << " forward: " << forward << std::endl;
             }
+
             // yaw --> right analog joystick
             if (sdlEvent.axis == 2) {
-                //mouse_xrel_ = sdlEvent.value;
+
+
+                //std::cout <<" sdlEvent.value: "<< sdlEvent.value << std::endl;
+
+                if (abs(sdlEvent.value) > 500) {
+                    mouse_xrel_ = 0.01 * fmaxf(-1.0f, (float) sdlEvent.value / MAX_VALUE_CONTROLLER);
+                }else {
+                    mouse_xrel_ = 0.0f;
+                }
+                /*
+                if ( abs(sdlEvent.value) > 1000 ) {
+                    mouse_xrel_ = sdlEvent.value;
+                }
+                 */
             }
             // pitch --> right analog joystick
             if (sdlEvent.axis == 3) {
-                //mouse_yrel_ = sdlEvent.value;
+
+                if (abs(sdlEvent.value) > 500) {
+                    mouse_yrel_ = 0.01*fmaxf(-1.0f, (float) sdlEvent.value / MAX_VALUE_CONTROLLER);
+                }else {
+                    mouse_yrel_ = 0.0f;
+                }
+                /*
+                if ( abs(sdlEvent.value) > 1000 ) {
+                    mouse_yrel_ = sdlEvent.value;
+                }
+                 */
             }
 
 
@@ -160,8 +171,8 @@ namespace sw {
             const Uint8 *currentKeyStates = SDL_GetKeyboardState(NULL);
 
             // Reset variables
-            mouse_xrel_ = mouse_yrel_ = 0;
-            forward = right = 0;
+
+            //forward = right = 0;
             is_sprinting = false;
 
 
@@ -195,7 +206,6 @@ namespace sw {
                     events.emit<QuitEvent>();
                 }
 
-
                 // Game Controller
                 switch (e.type) {
                     case SDL_CONTROLLERDEVICEADDED:
@@ -217,11 +227,14 @@ namespace sw {
                     default:
                         break;
                 }
+
+                /*
                 // Mouse
                 if (e.type == SDL_MOUSEMOTION) {
                     mouse_xrel_ = e.motion.xrel;
                     mouse_yrel_ = e.motion.yrel;
                 }
+                */
 
 
                 if (e.type == SDL_KEYDOWN) {
@@ -289,15 +302,26 @@ namespace sw {
                     }
                 }
             }
-            //std::cout << " forward: " << forward << std::endl;
-            //if ( forward > 0.1f || right > 0.1f)
-            if (forward != 0 || right != 0)
-                events.emit<MovementEvent>(right, forward, is_sprinting);
 
+            //std::cout <<" mouse_xrel_: "<< mouse_xrel_ << " " <<  mouse_yrel_ << std::endl;
+            //std::cout <<" ratio: "<< (float) mouse_xrel_ / (float) window_w << std::endl;
+            //std::cout << " forward: " << forward << std::endl;
+
+
+
+
+            if ( fabs(forward) > 0.5f || fabs(right) > 0.5f) {
+            //if (forward != 0 || right != 0)
+                events.emit<MovementEvent>(right, forward, is_sprinting);
+            }
             // If mouse moved
-            if (mouse_xrel_ != 0 || mouse_yrel_ != 0) {
-                events.emit<ViewChangedEvent>((float) mouse_xrel_ / (float) window_w,
+            if (fabs(mouse_xrel_) > 0.0002f || fabs(mouse_yrel_) > 0.0002f) {
+
+                /*events.emit<ViewChangedEvent>( (float) mouse_xrel_ / (float) window_w,
                                               (float) mouse_yrel_ / (float) window_w);
+                                              */
+                //std::cout << "för i helvete" << std::endl;
+                events.emit<ViewChangedEvent>(mouse_xrel_, mouse_yrel_);
             }
         }
 
@@ -309,7 +333,7 @@ namespace sw {
 
     private:
         float right, forward;
-        int mouse_xrel_, mouse_yrel_;
+        float mouse_xrel_, mouse_yrel_;
         int window_w = -1, window_h = -1;
 
         bool is_sprinting;
--- src/PlayerControlSystem.cpp
+++ src/PlayerControlSystem.cpp
@@ -156,7 +156,6 @@ void sw::PlayerControlSystem::receive(const MovementEvent &move) {
     move_right_ = move.right_;
     will_sprint = move.is_sprinting_;
     will_move_ = true;
-    std::cout << "move bitch " << std::endl;
 }
 
 void sw::PlayerControlSystem::receive(const ViewChangedEvent &view) {
